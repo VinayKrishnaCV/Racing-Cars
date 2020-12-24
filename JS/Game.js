6d28a8e44e396682cf3da9ede9c5bc8ebdf52365
Vinay Krishna C.V <vinaykrishnacv@gmail.com>
2020-12-24 18:51:42 +0530

    1st floor has images and endgame is there

--- JS/Game.js
+++ JS/Game.js
@@ -29,6 +29,10 @@ class Game {
             car2=createSprite(300,100,10,20)
             car3=createSprite(500,100,10,20)
             car4=createSprite(700,100,10,20)
+            car1.addImage(c1)
+            car2.addImage(c2)
+            car3.addImage(c3)
+            car4.addImage(c4)
             cars=[car1,car2,car3,car4]
         }
     }
@@ -36,27 +40,34 @@ class Game {
     play(){
         form.greeting.hide()
         Player.getPlayerInfo();
-        console.log(allPlayers)
-        var x=100;
+        var x=350;
         var y;
         var index = 0;
         for(var plr in allPlayers){
             //text(allPlayers[plr].name + " : " + allPlayers[plr].distance, 120, q)
+            y = displayHeight-180-allPlayers[plr].distance;
             cars[index].x = x;
-            cars[index].y = displayHeight-180-allPlayers[plr].distance;
+            cars[index].y = y;
             if(index===player.index-1){
                 cars[index].shapeColor="red"
                 camera.position.x=displayWidth/2
-                camera.position.y=cars[index].y
+                camera.position.y=cars[index].y-height/2+50
             }
             x+=200
             index+=1
         }
+        image(TRACK,0,-height*4,width,height*5)
         drawSprites()
         if(keyIsDown(UP_ARROW)){
             player.distance+=10
             player.update()
         }
-        
+        if(player.distance>2960){
+            gameState=2
+        }
+    }
+
+    end(){
+        console.log("game ended do not join again")
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 var gameState=0,playerCount,database,game,form,player,allPlayers;
-var cars,car1,car2,car3,car4
+var cars,car1,car2,car3,car4,c1,c2,c3,c4,TRACK
+
+
+function preload(){
+    c1=loadImage("Images/car1.png")
+    c2=loadImage("Images/car2.png")
+    c3=loadImage("Images/car3.png")
+    c4=loadImage("Images/car4.png")
+    TRACK=loadImage("Images/track.jpg")
+}
+
 
 function setup(){
 
@@ -15,11 +25,13 @@ function draw(){
 
     background("white");
 
-    if(playerCount===4){
+    if(playerCount===2){
         game.update(1)
     }
     if(gameState===1){
         game.play()
+    }if(gameState===2){
+        game.end()
     }
 
 }
